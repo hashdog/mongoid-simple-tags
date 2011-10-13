9756da0b481c5575ee7cef5c306a80cf77e6ccb0
Hendrik F. Louw <hendrik@qualica.com>
2011-10-13 14:43:49 +0200

    more cleanup

--- test/mongoid-simple-tags_test.rb
+++ test/mongoid-simple-tags_test.rb
@@ -63,22 +63,5 @@ describe "A Taggable model with tags assigned" do
 		end
 	end
 end
-#u = User.new(:name => "Tuquito")
-#u.tag_list = "linux, tucuman, free software"      
-#u.tags     # => ["linux","tucuman","free software"]
-#u.save
-
-#User.tagged_with("linux") # => u
-#User.tagged_with(["tucuman", "free software"]) # => u
-
-#u2 = User.new(:name => "ubuntu")
-#u2.tag_list = "linux"
-#u2.save
-
-#User.tagged_with("linux") # => [u, u2]
-
-#using map-reduce function
-#
-#User.all_tags #=>[{:name=>"free software", :count=>1}, {:name=>"linux", :count=>2}, {:name=>"tucuman", :count=>1}]
 
 
