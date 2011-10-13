55af5cf735de108ef2a218af3017cd7505a54a87
Hendrik F. Louw <hendrik@qualica.com>
2011-10-13 14:34:23 +0200

    added basic tests to reflect readme

--- test/helper.rb
+++ test/helper.rb
@@ -14,3 +14,16 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'mongoid'
 require 'mongoid-simple-tags'
+
+
+require 'yaml' 
+YAML::ENGINE.yamler= 'syck'
+
+
+#mongoid_config_path = File.join(Pathname.new(__FILE__).dirname, "mongoid_test.yml")
+#Mongoid.load!(mongoid_config_path)
+Mongoid.configure do |config|
+	config.master = Mongo::Connection.new.db("mongoid_simple_tags_test")
+end
+
+
--- test/mongoid-simple-tags_test.rb
+++ test/mongoid-simple-tags_test.rb
@@ -8,9 +8,59 @@ class User
 end
 
 describe "A Taggable model" do
+	before do
+		@user = User.new(name: 'Tuquito')
+	end
 
 	it "should have a tag_list method" do
+		@user.respond_to?(:tag_list)	
+	end
 
+	it "should be able to update a tag list" do
+		tag_list = "linux, tucuman, free software"
+		tags = tag_list.split(',').map{ |tag| tag.strip}.flatten
+		@user.tag_list = tag_list
+		assert_equal tags, @user.tags
+	end
+end
+
+describe "A Taggable model with tags assigned" do
+
+	before do 
+		@tag_list = "linux, tucuman, free software"
+		@user = User.new(name: 'Tuquito')
+		@user.tag_list = @tag_list
+		@user.save
+	end
+
+	it "should be able to find tagged_with objects" do 
+		assert_equal @user, User.tagged_with('linux').first
+		assert_equal @user, User.tagged_with(['tucuman', 'free software']).first
+	end
+
+	it "should be able to find tagged_with objects if more than one object is present" do
+					user2 = User.new(name: 'ubuntu')
+					user2.tag_list = "linux"
+					user2.save
+					tagged_with_list = User.tagged_with('linux')
+					assert tagged_with_list.include? @user
+					assert tagged_with_list.include? user2
+	end
+
+	it "should return all_tags per Model class" do
+					user2 = User.create(name: 'ubuntu', tag_list: 'linux')
+					all_tags_for_users = User.all_tags
+					expected_tag_list = [ {:name=>"free software", :count=>1},
+								 								{:name=>"linux", :count=>2},
+															 	{:name=>"tucuman", :count=>1}
+															]
+					assert_equal expected_tag_list, all_tags_for_users
+	end
+
+	after do 
+		Mongoid.database.collections.each do |collection|
+			collection.remove if collection.name !=~ /^system\./ 
+		end
 	end
 end
 #u = User.new(:name => "Tuquito")
